6d69d8e0fb094c2a548319f2425f9991673d1dce
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-27 16:58:56 +0800

    server.js: Added person api and express json middleware

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import connectDb from './db/connect.js'
 
+import person from './routes/person.js'
+
 // App config
 const app = express()
 const PORT = process.env.PORT || 8000
 
+// Middleware
+app.use( express.json() )
+
 app.get( '/', ( req, res ) => res.status(200).send('Health check') )
+
+// Routes
+app.use( '/api/v1/person', person )
 
 const start = async () => {
   try {
